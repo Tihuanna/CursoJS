e8d742b4d00fdbedafc326552045128985459ee5
Tihuanna <kevin_casapc@hotmail.com>
2022-07-17 22:11:04 -0300

    Update aula 4 comentários

---
+++ aula4/comentarios.js
@@ -0,0 +1,11 @@
+//Escrever um comentário
+
+console.log("Usando comentários")
+//Comentário de uma linha
+console.log("linha 1")
+console.log("linha 2")
+//Comentario
+
+/*
+Bloco de comentário
+*/
