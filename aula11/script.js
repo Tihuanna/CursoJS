13b25834a07c90495ced92ff6633c66e931e3f96
Tihuanna <kevin_casapc@hotmail.com>
2022-07-25 09:13:36 -0300

    Update até aula 15

---
+++ aula11/script.js
@@ -0,0 +1,4 @@
+let num1 = prompt ("Digite o primeiro numero")
+let num2 = prompt ("Digite o segundo numero")
+
+window.alert(`O resultado da soma é ${parseInt(num1) + parseInt(num2)}`)
---
+++ aula15/index.js
@@ -0,0 +1,10 @@
+//let num1 = 9.52312;
+//let numArredondado = Math.floor(num1);
+//let numArredondado2 = Math.ceil(num1);
+//let num2 = Math.round(num1);
+//console.log(numArredondado)
+//console.log(numArredondado2)
+//console.log(num2)
+let num1 = 9.52312;
+const aleatorio = Math.round(Math.random() * (10 - 1) + 1);
+console.log(aleatorio);
