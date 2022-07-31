e2486376159801f51583271bfa3ea13acffff87f
Tihuanna <kevin_casapc@hotmail.com>
2022-07-30 23:57:38 -0300

    Valores primitidos e referência

---
+++ Seção2 - Javascript básico/aula18/index.js
@@ -0,0 +1,28 @@
+function criaPessoa (nome, sobrenome, idade){
+    return{
+        nome: nome,
+        sobrenome: sobrenome,
+        idade: idade
+    }
+}
+
+const pessoa1 = criaPessoa('Kevin', 'Leal', 20);
+const pessoa2 = criaPessoa('João', 'Silva', 30);
+const pessoa3 = criaPessoa('Maria', 'Souza', 40);
+
+console.log(pessoa1);
+console.log(pessoa2);
+console.log(pessoa3);
+
+
+const pessoa4 = {
+    nome: 'Kevin',
+    sobrenome: 'Leal',
+    idade: 20,
+
+    fala(){
+    console.log(`Meu nome é ${this.nome} ${this.sobrenome} e tenho ${this.idade} anos`)
+    }
+}
+
+pessoa4.fala();
